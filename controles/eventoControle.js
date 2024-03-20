8d138aa351a4870ecc29e365f34d3e5057a459b1
Gabriel Pugas <gabrielpugas@gmail.com>
2024-03-19 22:24:30 -0300

    controller

--- controles/eventoControle.js
+++ controles/eventoControle.js
@@ -2,19 +2,19 @@ export default class ClienteControle {
     gravar(requisicao, resposta) {
         resposta.type('application/json');
         if (requisicao.method === 'POST' && requisicao.is('application/json')) {
-            const titulo = requisicao.titulo;
-            const descricao = requisicao.descricao;
-            const data = requisicao.data;
-            const horario = requisicao.horario;
-            const local = requisicao.local;
-            const precoIngresso = requisicao.precoIngresso;
-            const ingressosDisponiveis = requisicao.ingressosDisponiveis;
+            const titulo = requisicao.body.titulo;
+            const descricao = requisicao.body.descricao;
+            const data = requisicao.body.data;
+            const horario = requisicao.body.horario;
+            const local = requisicao.body.local;
+            const precoIngresso = requisicao.body.precoIngresso;
+            const ingressosDisponiveis = requisicao.body.ingressosDisponiveis;
             if (titulo && descricao && data && horario && local && precoIngresso && ingressosDisponiveis) {
                 const evento = new Evento(0, titulo, descricao, data, horario, local, precoIngresso, ingressosDisponiveis);
                 evento.gravar().then((id) => {
                     resposta.status(201).json({
                         status: 'ok',
-                        mensagem: 'Registro gravado com sucesso. ID: ' + id;
+                        mensagem: 'Registro gravado com sucesso. ID: ' + id
                     });
                     
                 }).catch((erro) => {
@@ -41,15 +41,39 @@ export default class ClienteControle {
     atualizar(requisicao, resposta) {
         resposta.type('application/json');
         if ((requisicao.method === 'PUT' || requisicao.method === 'PATCH') && requisicao.is('application/json')) {
-            const id = requisicao.id;
-            const titulo = requisicao.titulo;
-            const descricao = requisicao.descricao;
-            const data = requisicao.data;
-            const horario = requisicao.horario;
-            const local = requisicao.local;
-            const precoIngresso = requisicao.precoIngresso;
-            const ingressosDisponiveis = requisicao.ingressosDisponiveis;
-            
+            const id = requisicao.params.id;
+            const titulo = requisicao.body.titulo;
+            const descricao = requisicao.body.descricao;
+            const data = requisicao.body.data;
+            const horario = requisicao.body.horario;
+            const local = requisicao.body.local;
+            const precoIngresso = requisicao.body.precoIngresso;
+            const ingressosDisponiveis = requisicao.body.ingressosDisponiveis;
+            if (id>0 && titulo && descricao && data && horario && local && precoIngresso && ingressosDisponiveis) {
+                const evento = new Evento(id, titulo, descricao, data, horario, local, precoIngresso, ingressosDisponiveis);
+                evento.atualizar().then(() => {
+                    resposta.status(200).json({
+                        status: 'ok',
+                        mensagem: 'Evento atualizado com sucesso.'
+                    });
+                    
+                }).catch((erro) => {
+                    resposta.status(500).json({
+                        status: 'erro',
+                        mensagem: 'Erro ao atualizar o registro. Consulte API para mais detalhes.'
+                    })
+                });
+            } else {
+                resposta.status(400).json({
+                    status: 'erro',
+                    mensagem: 'Todos os campos devem ser informados. Consulte API para mais detalhes.'
+                });
+            }
+        } else {
+            resposta.status(405).json({
+                status: 'erro',
+                mensagem: 'Requisição inválida. Consulte API para mais detalhes.'
+            });
         }
     }
 
